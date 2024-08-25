18a998e610fe2b4fa3244a9de72982929029092e
pushpakgupta <pushpak.gupta2@gmail.com>
2024-08-25 17:06:11 +0530

    new analytics

--- app/routes/app.inventory.jsx
+++ app/routes/app.inventory.jsx
@@ -4,22 +4,22 @@
 
 
 
-// import { useLoaderData } from '@remix-run/react';
+// // import { useLoaderData } from '@remix-run/react';
 
-// export const loader = async () => {
-//   const response = await fetch(`${process.env.BASE_URL}/api/products`); // Calling your internal API
-//   return response.json();
-// };
+// // export const loader = async () => {
+// //   const response = await fetch(`${process.env.BASE_URL}/api/products`); // Calling your internal API
+// //   return response.json();
+// // };
 
-export default function Products() {
+// export default function Products() {
 
 
-  return (
-    <div>
-      <h1>Products List</h1>
-      <ul>
-       <li>Yash</li>
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Products List</h1>
+//       <ul>
+//        <li>Yash</li>
+//       </ul>
+//     </div>
+//   );
+// }
